a7fd5aa911a96089eb231f4c04eab22bc64fafb7
汪然 <wang99ran@163.com>
2023-02-13 16:11:49 +0800

    完善mybatis-plus的代码生成配置

--- src/main/java/com/laowang/springboot3template/CodeGenerator.java
+++ src/main/java/com/laowang/springboot3template/CodeGenerator.java
@@ -2,6 +2,7 @@ package com.laowang.springboot3template;
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.TemplateType;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.util.Collections;
@@ -22,19 +23,27 @@ public class CodeGenerator {
         FastAutoGenerator.create(url, username, password)
                 .globalConfig(builder -> {
                     builder.author("WangRan") // 设置作者
-                            .enableSwagger() // 开启 swagger 模式
+//                            .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
                             .outputDir(projectPath + "/src/main/java"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
                     builder.parent("com.laowang.springboot3template") // 设置父包名
-                            .moduleName("system") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D://")); // 设置mapperXml生成路径
+//                            .moduleName("system") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, projectPath + "/src/main/resources/mapper")); // 设置mapperXml生成路径
                 })
-//                .strategyConfig(builder -> {
+                .strategyConfig(builder -> {
 //                    builder.addInclude("t_simple") // 设置需要生成的表名
 //                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
-//                })
+
+                    builder.entityBuilder().enableLombok();
+                    builder.controllerBuilder().enableRestStyle().enableHyphenStyle();
+                    builder.mapperBuilder().enableBaseColumnList().enableBaseResultMap();
+                })
+                .templateConfig(builder -> {
+                    //禁止除entiy的模板，只更新实体类
+//                    builder.disable(TemplateType.CONTROLLER,TemplateType.MAPPER,TemplateType.SERVICE, TemplateType.SERVICEIMPL,TemplateType.XML);
+                })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
     }
